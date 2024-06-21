d26433ca418c8ec01a3d2274c250f9694edc9ef7
Mark Gotham <mark.r.gotham@durham.ac.uk>
2024-06-21 15:20:51 +0100

    Merge shared and remove duplicate func

--- code/utils.py
+++ code/utils.py
@@ -1,9 +1,36 @@
 import inflect
 import re
-from music21 import *
-import pandas as pd
 import numpy as np
 p = inflect.engine()
+from pathlib import Path
+
+CODE_PATH = Path(__file__).parent
+REPO_PATH = CODE_PATH.parent
+CORPUS_PATH = REPO_PATH / "corpus"
+
+
+def get_corpus_files(
+    sub_corpus_path: Path = CORPUS_PATH,
+    file_name: str = "*.mxl",
+) -> list[Path]:
+    """
+    Get and return paths to files matching conditions for the given file_name.
+
+    Args:
+        sub_corpus_path: the sub-corpus to run.
+            Defaults to CORPUS_PATH (all corpora).
+            Accepts any sub-path thereof.
+            Checks ensure both that the path `.exists()` and `.is_relative_to(CORPUS_FOLDER)`
+        file_name (str): select all files matching this file_name. Defaults to "score.mxl".
+        Alternatively, specify either an exact file name or
+        use the wildcard "*" to match patterns, e.g., "*.mxl" for all .mxl files
+
+    Returns: list of file paths.
+    """
+
+    assert sub_corpus_path.is_relative_to(CORPUS_PATH)
+    assert sub_corpus_path.exists()
+    return [x for x in sub_corpus_path.rglob(file_name)]
 
 
 def find_nearest(arr, x):
@@ -45,73 +72,3 @@ def sort_by_pitch(all_notes):
 
     return sorted_notes
     # TODO: edit lambda function so this is not necessary
-
-
-def convert_mxml_to_csv(file, output_file):
-    """
-    Convert mxml -> csv with this format:
-    qstamp | bar | beat | instrument1 | instrument2 | instrument3 |
-
-    - if note lasts longer than a qstamp, it will be repeated in the next row
-    - scientific pitch notation
-    - bar is integer
-    - qstamp and beat are floats, depends on the rhythm, increment may not be constant
-        e.g. (1, 2, 2.5, 3, 4, 5, 6, 7.25, 7.5, 7.75, 8)
-    - if chord, cell will contain array of notes
-    - if qstamp exists, find current instrument column and rewrite cell
-    - r to represent rest
-    """
-
-    score = converter.parse(file)
-    parts = instrument.partitionByInstrument(score)
-    print(len(parts))
-    instruments = [part.getInstrument().instrumentName for part in parts.parts]
-
-    df = pd.DataFrame(columns=['qstamp', 'bar', 'beat'] + instruments)
-
-    for part in parts:
-        print(part)
-        instrument_name = part.getInstrument().instrumentName
-        notes = part.flat.notes
-        for note in notes:
-            if note.isChord:  # array of notes as strings
-                df[instrument_name] = df[instrument_name].astype(object)
-                notes_array = [chord_note.nameWithOctave for chord_note in note]
-                print(notes_array)
-                if note.offset in df['qstamp'].values:
-                    try:
-                        df.loc[df['qstamp'] == note.offset, instrument_name] = str(notes_array)
-                    except ValueError as e:
-                        print("ValueError: could not assign chord to qstamp")
-                        print(df[df['qstamp'] == note.offset])
-                        print(df.loc[df['qstamp'] == note.offset, instrument_name])
-                        print("note.offset:", note.offset)
-                        print("df['qstamp'].values:", df['qstamp'].values)
-                        print(notes_array)
-                        print(e)
-                        input()
-
-                else:
-                    df = df.append({'qstamp': note.offset, 'bar': note.measureNumber, 'beat': note.beat,
-                                    instrument_name: notes_array}, ignore_index=True)
-            else:  # string of single note
-                if note.offset in df['qstamp'].values:
-                    df.loc[df['qstamp'] == note.offset, instrument_name] = note.nameWithOctave
-                else:
-                    new_row = {'qstamp': note.offset,
-                               'bar': note.measureNumber,
-                               'beat': note.beat,
-                               instrument_name: note.nameWithOctave}
-                    df = df.append(new_row, ignore_index=True)
-
-    df = df.fillna(method='ffill')
-
-    df = df.sort_values(by=['qstamp', 'bar', 'beat'])
-    df.to_csv(output_file, index=False)
-    return df
-
-
-
-
-
-
