fcb27feb9350d9b2c23f13fe32981e9d3c5a824e
Mark Gotham <mark.r.gotham@durham.ac.uk>
2024-08-01 11:00:40 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,14 @@
 from pathlib import Path
+TEST_DIR = Path(__file__).parent
+REPO_DIR = TEST_DIR.parent
+import sys
+sys.path.append(REPO_DIR / "code")
+
+
 import unittest
-from code import CODE_PATH, CORPUS_PATH, hauptstimme
+from code.hauptstimme import *
+from code.orchestra_part_split import *
+from music21 import instrument
 
 
 class Test(unittest.TestCase):
@@ -11,11 +19,16 @@ class Test(unittest.TestCase):
         including a test of all 3 partForTemplate formats (int, str, instrument object).
         """
 
-        p = CORPUS_PATH / "sq"
-        f = "score.mxl"
+        p = TEST_DIR / "score.mxl"
 
-        # for i in [8, 'violin', instrument.Violin()]:
-        hauptstimme.process_one(p, f, outPathData=p, outPathScore=p)  #, partForTemplate=i)
+        for i in [8, 'violin', instrument.Violin()]:
+            process_one(p, part_for_template=i)
+
+    def test_accent_to_sf(self):
+        clean_up(TEST_DIR / "accent_test.mxl", map_accent_to_sf = True)
+
+    def split(self):
+        split_one(file_name_out="split_test_score_out.mxl")
 
 
 # ------------------------------------------------------------------------------
